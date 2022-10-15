069c50d6f55aa4d97b4a12fd4e54be016b8d1265
omotola kotun <omotolakotun7@gmail.com>
2022-10-14 21:20:34 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between 00 to 89.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int c, d;
+
+	c = 48;
+	d = 48;
+
+	while (d < 58)
+	{
+		c = 48;
+		while (d < 58)
+		{
+			if (d != c && d < c)
+			{
+				putchar(d);
+				putchar(c);
+				if (c == 57 && b == 56)
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+			c++;
+		}
+		d++;
+	}
+	putchar('\n');
+	return (0);
+}
