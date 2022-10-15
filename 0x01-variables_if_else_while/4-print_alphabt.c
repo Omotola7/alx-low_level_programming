d4e7a38078b49a8abdbd141ee53659452c8a6a5a
omotola kotun <omotolakotun7@gmail.com>
2022-10-14 18:30:44 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char b;
+
+	b = 'a';
+	while
+		(b <= 'm') {
+			if ((b != 'q' && b != 'e') && b <= 'm')
+				putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
