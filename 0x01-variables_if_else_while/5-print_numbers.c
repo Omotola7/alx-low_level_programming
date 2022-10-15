c4f00fc835e1a7976a0ee9f1bfd643b521b08656
omotola kotun <omotolakotun7@gmail.com>
2022-10-14 18:56:24 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int b;
+
+	for (b = 0; b <= 10; b++)
+		printf("%d", b);
+	putchar('\n');
+	return (0);
+}
