3b5170f1afb497d80ba306dee4b2614d26cbf95e
omotola kotun <omotolakotun7@gmail.com>
2022-10-14 20:24:08 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: to 0
+ */
+
+int main(void)
+{
+	char b;
+
+	int e;
+
+	b = 'a';
+	while
+		(e < 10)
+		{
+			putchar(e + '0');
+			e++;
+		}
+	while
+		(b <= 'f')
+		{
+			putchar(b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
