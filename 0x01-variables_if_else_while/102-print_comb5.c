235b743b89025533c153ec8e29c5cf46998be1c8
omotola kotun <omotolakotun7@gmail.com>
2022-10-17 15:05:59 -0700

    Task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: to 0
+ */
+
+int main(void)
+{
+	int l;
+	int m;
+
+	for (l = 0; m < 99; l++)
+	{
+		for (m = l + 1; m < 100; m++)
+		{
+			putchar((l / 10) + '0');
+			putchar((l % 10) + '0');
+
+			putchar(' ');
+
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (h == 98 && i == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
