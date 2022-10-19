24362d6534ceb8254fbc23365c3c9c2f9cfe89f2
omotola kotun <omotolakotun7@gmail.com>
2022-10-18 22:35:31 -0700

    Task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <studio.h>
+/**
+ * main - main bloacks
+ * Description: print the sum of even fibonacci numbers up to a fib value
+ * not exceeding 4,000,000.
+ * Return: 0
+ */
+int main(void)
+{
+	int e = 1, m = 2, total = 0;
+	int o;
+
+	while (m < 4000000)
+	{
+		if (m % 2 == 0)
+			total += m;
+		o = m;
+		m += e;
+		e = o;
+	}
+	printf("%d\n", total);
+	return (0);
+}
