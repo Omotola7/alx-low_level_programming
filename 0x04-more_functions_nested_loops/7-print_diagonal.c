7fa5d15367b9496c3204f18a1d36fc84fb39f261
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 19:39:53 -0700

    7 print_diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: numbers of '\' to be printed
+ */
+void print_diagonal(int n)
+{
+	int b, c;
+
+	if (n <= 0)
+		_putchar('\n');
+}
