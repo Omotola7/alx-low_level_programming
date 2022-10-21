a5b5422c79e733a5859f85a3b4d9046477c22745
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 18:36:45 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiply two integers
+ * @k: first int
+ * @l: second int
+ * Return: the multiplication of k and l
+ */
+int mul(int k, int l)
+{
+	return (k * l);
+}
