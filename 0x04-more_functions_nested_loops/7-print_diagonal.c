48091b56a354a8fc5813add850e727f538daec44
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 19:58:09 -0700

    7 print_diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,8 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int b, c;
+	int k, l;
 
 	if (n <= 0)
 		_putchar('\n');
+	for (k = 0; k < n; k++)
+	{
+		for (l = 0; l < k; l++)
+			_putchar(' ');
+
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
