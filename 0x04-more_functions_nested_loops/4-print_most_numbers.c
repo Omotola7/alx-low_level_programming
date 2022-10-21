fbec7033f3d73f5e024479c0a9f0345e577e6637
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 19:07:18 -0700

    4 print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers 0 to 9
+ * without 2 and 4
+ * Return: Always 0
+ */
+void print_most_numbers(void)
+{
+	int k = 0;
+
+	while (k < 10)
+	{
+		if (k != 2 && a != 4)
+			_putchar(k + '0');
+
+		k++;
+	}
+	_putchar('\n');
+}
