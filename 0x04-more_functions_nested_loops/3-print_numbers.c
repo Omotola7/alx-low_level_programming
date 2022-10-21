7440b95428e4d01b6add2416cb98568eced08d76
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 18:45:49 -0700

    3-print number

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print from 0 to 9
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int k = 0;
+
+	while (k < 10)
+		_putchar(k++ + '0');
+
+	_putchar('\n');
+}
