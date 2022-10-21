89e6f8432c59ba251dff3ce709a164c144103a37
omotola kotun <omotolakotun7@gmail.com>
2022-10-20 19:24:24 -0700

    5 more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 to 14 ten times
+ * Return: Always 0
+ */
+void more_numbers(void)
+{
+	int num, k;
+
+	for (k = 0; k <= 9; k++)
+	{
+		for (num = 0; num <= 14; num++)
+		{
+			if (num > 9)
+			{
+				_putchar((num / 10) + '0');
+			}
+			_putchar((num % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
