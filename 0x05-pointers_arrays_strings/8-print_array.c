5463f8fac03d8f8a08d7c71f7299991ab63daa0a
omotola kotun <omotolakotun7@gmail.com>
2022-10-24 15:30:11 -0700

    Task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints n elements of an array
+ * @a: array
+ * @n: number of values to be printed
+ */
+void print_array(int *a, int n)
+{
+	int l;
+
+	for (l = 0; l < n; l++)
+	{
+		printf("%", a[l]);
+		if (l != n - 1)
+			printf(", ");
+	}
+
+	printf("\n");
+}
