60674995abc74719d778146b38fa4f11f11256e0
omotola kotun <omotolakotun7@gmail.com>
2022-10-27 13:55:03 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,9 +18,9 @@ char *_strncat(char *dest, char *src, int n)
 		i++;
 	}
 	j = 0;
-	while (j < n && scr[j] != '\0')
+	while (j < n && src[j] != '\0')
 	{
-		dest[i] = scr[j];
+		dest[i] = src[j];
 		i++;
 		j++;
 	}
