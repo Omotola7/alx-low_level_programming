20691e482dcc6c2e9b13a715f0347288ff6e369e
omotola kotun <omotolakotun7@gmail.com>
2022-11-10 18:25:05 -0800

    string nconcat

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * string_nconcat - concatenates two strings.
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: char ponter
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *f;
+	unsigned int size1 = 0, size2 = 0, l;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[size1] != '\0')
+	{
+		size1++;
+	}
+
+	while (s2[size2] != '\0')
+	{
+		size2++;
+	}
+
+	if (f == NULL)
+		return (0);
+
+	for (l == 0; l < size1; l++)
+	{
+		f[l] = s1[l];
+	}
+
+	for (; l < (size1 + n); l++)
+	{
+		f[l] = s2[l - size1];
+	}
+	f[l] = '\0';
+
+return (f);
+}
