2f42afc3c921733956325647aa5f71da2c617fc8
omotola kotun <omotolakotun7@gmail.com>
2022-11-17 15:54:12 -0800

    Task3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -27,7 +27,7 @@ void print_all(const char * const format, ...)
 				break;
 			  } k++;
 		}
-		switch (format[l])
+		switch (format[i])
 		{
 		case 'c':
 			printf("%c", va_arg(valist, int)), c = 1;
